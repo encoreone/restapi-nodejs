516e0b93a99f5c0704e7f169bb33dbb070bbf0ac
encoreone <thetinxone@gmail.com>
2022-06-30 12:03:26 +0500

    create routes and controllers

---
+++ README.md
@@ -0,0 +1,14 @@
+<h1>RestApi on Node.js<h1>
+<h2>How to start?</h2>
+<h3>npm i && npm start</h3>
+
+CRUD app: 
+    POST (create) - /posts 
+    with params (author, title, content, picture)
+
+    GET (read all) - /posts
+
+    GET (read one) - /posts/id
+
+    PUT (update) - /posts
+    DELETE (delete) - /posts/id
--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import router from './Route.js';
+import fileUpload from 'express-fileupload';
 
 const PORT = 5000;
+const DB_URL = 'mongodb://root:root@localhost:27017/';
 const app = express();
 
-app.use(express.json())
-app.get('/', (req, res) => {
-    req.query
-    res.status(200).json('Сервер работает!!!')
-})
+app.use(express.json());
+app.use(fileUpload({}));
+app.use(express.static('static'));
+app.use('/api', router);
 
-app.listen(PORT, () => console.log(`Сервер стартовал на ${PORT} порту`))
+async function startApp() {
+    try {
+        mongoose.connect(DB_URL)
+        app.listen(PORT, () => console.log(`Сервер стартовал на ${PORT} порту`));
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+startApp();
